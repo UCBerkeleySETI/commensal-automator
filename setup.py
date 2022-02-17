f149c35577ca752ad2e767540e94f83d82be6379
Daniel <daniel.czech@protonmail.com>
2022-02-17 19:59:49 +0200

    Installation script.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+requires = [
+    'numpy >= 1.18.1',
+    'redis >= 3.4.1',
+    ]
+
+setuptools.setup(
+    name = 'automator',
+    version = '1.0',
+    url = 'https://github.com/UCBerkeleySETI/commensal-automator',
+    license = 'MIT',
+    author = 'Daniel Czech',
+    author_email = 'danielc@berkeley.edu',
+    description = 'Automation for Breakthrough Listen\'s commensal observing',
+    packages = [
+        'automator',
+        ],
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            'automator = automator.cli:cli',
+            ]
+        },
+    )
