ffac8c068901a1c4078e33e6dee65c2a621f2a32
Daniel <daniel.czech@protonmail.com>
2022-11-11 15:16:26 +0200

    Add the ability to specify process to start/stop scripts.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 SCRIPTS_DIR=$(dirname $0)
+PROCESS_NAME=$1
 $SCRIPTS_DIR/check_env.sh
+
+if [ "$PROCESS_NAME" != "coordinator" ] && [ "$PROCESS_NAME" != "automator" ] 
+then
+    echo "Process can be either: automator OR coordinator"
+    exit 0
+fi
+
 USER=`whoami`
-MESSAGE="$USER is manually starting the automator."
+MESSAGE="$USER is manually starting the $PROCESS_NAME."
 python $SCRIPTS_DIR/publish_to_slack.py --slack_channel=meerkat-obs-log --message="$MESSAGE"
-$SCRIPTS_DIR/circus.sh start automator
+$SCRIPTS_DIR/circus.sh start $PROCESS_NAME
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -e
 SCRIPTS_DIR=$(dirname $0)
+PROCESS_NAME=$1
 $SCRIPTS_DIR/check_env.sh
+
+
+if [ "$PROCESS_NAME" != "coordinator" ] && [ "$PROCESS_NAME" != "automator" ] 
+then
+    echo "Process can be either: automator OR coordinator"
+    exit 0
+fi
+
 USER=`whoami`
-MESSAGE="$USER is manually stopping the automator."
+MESSAGE="$USER is manually stopping the $PROCESS_NAME."
 python $SCRIPTS_DIR/publish_to_slack.py --slack_channel=meerkat-obs-log --message="$MESSAGE"
-$SCRIPTS_DIR/circus.sh stop automator
+$SCRIPTS_DIR/circus.sh stop $PROCESS_NAME
