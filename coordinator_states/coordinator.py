36bd05ccdc7cee07929887e23d9d0cd7e8bbff31
daniel <daniel.czech@protonmail.com>
2023-05-24 18:06:52 +0100

    Beginnings of coordinator state machine controller.

---
+++ coordinator_states/coordinator.py
@@ -0,0 +1,28 @@
+from automator import util, redis_util
+from automator.logger import log
+
+class Coordinator(object):
+    """Coordinator that runs on the headnode and allocates instances to
+    recording and processing tasks for each subarray. 
+    """
+
+    def __init__(self, config_file):
+
+        config = util.load_config(config_file)
+        self.free = config['instances']
+        self.streams_per_instance = config['streams_per_instance']
+        self.r = redis.StrictRedis(host=config[], 
+                                   port=config[], 
+                                   decode_responses=True)
+
+    def start(self):
+        """Start the coordinator.
+        """
+        self.alert('starting up')
+
+    def alert(self, message):
+        redis_util.alert(self.red, message, "coordinator")
+    
+    def annotate(self, tag, text):
+        response = util.annotate_grafana(tag, text)
+        log.info(f"Annotating Grafana, response: {response}")
