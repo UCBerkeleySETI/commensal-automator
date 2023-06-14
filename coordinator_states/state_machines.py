cf171f970aebe798639211b2a02597c89d289d62
daniel <daniel.czech@protonmail.com>
2023-06-14 01:00:38 +0100

    Basic state machine classes for recproc and freesubscribed.

---
+++ coordinator_states/state_machines.py
@@ -0,0 +1,37 @@
+"""
+State machine class for use with the coordinator.
+"""
+
+
+class FreeSubscribedMachine(object):
+
+    def __init__(self, initial_state, free, subscribed):
+
+        self.state = initial_state
+
+        self.data = {
+            "free":free,
+            "subscribed":subscribed
+        }
+
+    def handle_event(self, event):
+         self.state = current_state.handle_event(event, data)
+
+
+class RecProcMachine(object):
+
+    def __init__(self, initial_state, all_instances, subscribed):
+
+        self.state = initial_state
+        
+        self.data = {
+            "subscribed":subscribed
+            "ready":set(),
+            "recording":set(),
+            "processing":set()
+        }
+
+        self.data[initial_state.name] = all_instances 
+
+    def handle_event(self, event):
+         self.state = current_state.handle_event(event, data)
