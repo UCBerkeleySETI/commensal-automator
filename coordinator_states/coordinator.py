9294e1109e983385db9c7667f03df5f4c37d33f0
daniel <daniel.czech@protonmail.com>
2023-06-14 01:02:55 +0100

    Initial full version of the coordinator.

--- coordinator_states/coordinator.py
+++ coordinator_states/coordinator.py
@@ -3,6 +3,9 @@ import redis
 from automator import util, redis_util
 from automator.logger import log
 
+from states import Ready, Free
+from state_machines import RecProcMachine, FreeSubscribedMachine
+
 class Coordinator(object):
     """Coordinator that runs on the headnode and allocates instances to
     recording and processing tasks for each subarray. 
@@ -11,16 +14,57 @@ class Coordinator(object):
     def __init__(self, config_file):
 
         config = util.load_config(config_file)
-        self.free = config['instances']
-        self.streams_per_instance = config['streams_per_instance']
-        self.r = redis.StrictRedis(host=config['redis_host'], 
-                                   port=config['redis_port'], 
+        self.channels = config["channels"]
+        self.free = set(config["instances"])
+        self.arrays = config["arrays"]
+        self.r = redis.StrictRedis(host=config["redis_host"],
+                                   port=config["redis_port"],
                                    decode_responses=True)
+        self.recproc_machines = dict()
+        self.freesubscribed_machines = dict()
+        self.subscribed = dict()
 
     def start(self):
         """Start the coordinator.
         """
-        self.alert('starting up')
+        self.alert("Starting up")
+
+        for array in self.arrays:
+
+            # TODO: Detect current state for array and select initial states.
+            # For now, assume we will start in READY and FREE
+
+            self.subscribed[array] = set()
+
+            self.freesubscribed_machines[array] = FreeSubscribedMachine(Free(array, self.r), self.free, self.subscribed[array])
+            self.recproc_machines[array] = RecProcMachine(Ready(array, self.r), self.free.copy(), self.subscribed[array])
+
+        # Listen for events and respond:
+
+        ps = self.redis_server.pubsub(ignore_subscribe_messages=True)
+        ps.subscribe(self.channels)
+
+        for message in ps.listen():
+            # TODO: Richer message parsing and review of alerts channel messages.
+            # TODO: Decide about how to manage recording_complete messaging
+            # (watcher-style process for each instance, or timer in rec_util?)
+            components = redis_util.parse_msg(message)
+            if components:
+                if components[0] == "RETURN":
+                    self.processing_return(message)
+                else:
+                    array = components[0]
+                    event = component[1]
+                    self.freesubscribed_machines[array].state.handle_event(event)
+                    self.recproc_machines[components[0].state.handle_event(event)
+
+    def processing_return(self, message):
+        """Note, we must return these to every array's state machine for the
+        moment until we start using Redis hashes for instance-specific
+        communication.
+        """
+        for machine in self.recproc_machines.values():
+            machine.state.handle_event(message)
 
     def alert(self, message):
         redis_util.alert(self.red, message, "coordinator")
