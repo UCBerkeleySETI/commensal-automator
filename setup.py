7d15b2ba6144d44e1f0b565cb3baf516598fa81a
Daniel <daniel.czech@protonmail.com>
2023-07-06 15:43:06 +0200

    Change package name and add requests to requirements.

--- setup.py
+++ setup.py
@@ -3,23 +3,21 @@ import setuptools
 requires = [
     'numpy >= 1.18.1',
     'redis >= 3.4.1',
+    'requests == 2.28.1',
     'katsdptelstate >= 0.11',
     'PyYAML >= 6.0',
     'pyzmq >= 25.0.0'
     ]
 
 setuptools.setup(
-    name = 'automator',
+    name = 'coordinator',
     version = '2.0',
     url = 'https://github.com/UCBerkeleySETI/commensal-automator',
     license = 'MIT',
     author = 'Daniel Czech, Dave MacMahon, Kevin Lacker',
     author_email = 'danielc@berkeley.edu',
     description = 'Automation for Breakthrough Listen\'s commensal observing',
-    packages = [
-        'coordinator',
-        'bluse_analyzer'
-        ],
+    packages = ['coordinator'],
     install_requires=requires,
     entry_points = {
         'console_scripts':[
